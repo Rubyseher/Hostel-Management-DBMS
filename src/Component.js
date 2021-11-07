0143e7cac612b98c8532870bd2ee15779645ae35
rubyseher <rubyseher2002@gmail.com>
2021-11-08 00:16:05 +0530

    student card

--- src/Component.js
+++ src/Component.js
@@ -26,8 +26,17 @@ export const StudentCards = props => {
     if (redirect) return <Redirect push to={{ pathname: redirect }} />
 
     return (
-        <div className='studentCard'>
-            <PersonIcon sx={{ fontSize: 26 }} />
+        <div style={{margin:'5px 15px'}}>
+            <div className='studentCard'>
+                <div className='circleS'><PersonIcon sx={{ fontSize: 32, color: "white",margin:'3px 0px 0 5px' }} /></div>
+                <h5 style={{ position: 'relative', left: 60, top: -25 }}>John Snow</h5>
+                <h6><b>Student id :</b> ENG20C0265</h6>
+                <h6><b>Father Name :</b> Ajay</h6>
+                <h6><b>Dept :</b> CSE</h6>
+                <h6><b>Dob :</b> 2002-09-30</h6>
+                <h6><b>Age :</b>18</h6>
+                <h6><b>City :</b> Bangalore</h6>
+            </div>
         </div>
     )
 }
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,20 @@
 import React, { Component, useState } from "react";
 import './Components.css'
-import { Navbars,StudentCards } from './Component.js';
+import { Navbars, StudentCards } from './Component.js';
 
 function Dashboard() {
     return (
-        <div>
+        <div style={{ backgroundColor: '#e7f3fe' }}>
             <Navbars />
-            <div style={{display:"inline-block",padding:40}}>
+            <div style={{ display: "inline-block", padding: 40, marginLeft:250, width:'80%' }}>
                 <h2>Students</h2>
-                <StudentCards/>
+                <div className="horizontalScroll">
+                    <StudentCards />
+                    <StudentCards />
+                    <StudentCards />
+                    <StudentCards />
+                </div>
+                <br/>
                 <h2>Visitors</h2>
                 <h2>Staff</h2>
             </div>
